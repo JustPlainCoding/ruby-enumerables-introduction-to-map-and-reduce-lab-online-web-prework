9b113fc45b6c2754e575dc08aa46c8c4cdbf7bb4
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-17 23:19:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,7 +49,18 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
+  counter = 0
+  result = false
+  while counter < source_array.length
+  total += source_array[counter]
+  counter += 1
+  end
 end
 
 def reduce_to_any_true(source_array)
+  counter = 0
+  while counter < source_array.length
+  total += source_array[counter]
+  counter += 1
+  end
 end
