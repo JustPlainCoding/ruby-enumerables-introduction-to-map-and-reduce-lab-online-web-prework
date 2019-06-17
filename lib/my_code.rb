f7e7be9c1cf3d360b3eabdef235120b10638139a
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-17 23:14:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def map_to_no_change(source_array)
   counter = 0
   new_array = []
   while counter < source_array.length
-  new_array << source_array[counter] * -1
+  new_array << source_array[counter]
   counter += 1
   end
   new_array
@@ -22,20 +22,20 @@ def map_to_double(source_array)
   counter = 0
   new_array = []
   while counter < source_array.length
- new_array<< source_array[counter] * -1
+  new_array<< source_array[counter] * 2
   counter += 1
   end
-  negative_array
+  new_array
 end
 
 def map_to_square(source_array)
   counter = 0
-  negative_array = []
+  new_array = []
   while counter < source_array.length
-  negative_array << source_array[counter] * -1
+  new_array << source_array[counter] * source_array[counter]
   counter += 1
   end
-  negative_array
+  new_array
 end
 
 def reduce_to_total(source_array, starting_point)
